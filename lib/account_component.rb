c67ebd55b1c87c34418521db33060c9afcfdce00
Scott Bellware <sbellware@gmail.com>
2017-04-04 12:36:23 -0500

    Deposit messages and handler

--- lib/account_component.rb
+++ lib/account_component.rb
@@ -1,1 +1,5 @@
 require 'eventide/postgres'
+
+require 'account_component/messages/commands/deposit'
+require 'account_component/messages/events/deposited'
+require 'account_component/handlers/commands'
