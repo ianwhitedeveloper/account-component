5675e140b14940fa4ecfb838ef2859c5383343e5
ntl <nathanladd+github@gmail.com>
2017-04-05 17:39:20 -0500

    Account deposit exercise

---
+++ exercise/account_deposit.rb
@@ -0,0 +1,13 @@
+require_relative './exercise_init'
+
+account = Account.new
+account.id = "123"
+account.balance = 1
+
+puts "Account #{account.id} balance: $#{account.balance}"
+
+amount = 11
+puts "Deposit $#{amount} to account #{account.id}"
+account.deposit(amount)
+
+puts "Account #{account.id} balance: $#{account.balance}"
