762fbc34f31d64b8c55deeeede8d4dd57fdea92d
ntl <nathanladd+github@gmail.com>
2017-04-06 16:13:02 -0500

    Withdrawal starting point

--- lib/account_component.rb
+++ lib/account_component.rb
@@ -1,7 +1,9 @@
 require 'eventide/postgres'
 
 require 'account_component/messages/commands/deposit'
+require 'account_component/messages/commands/withdraw'
 require 'account_component/messages/events/deposited'
+require 'account_component/messages/events/withdrawn'
 require 'account_component/handlers/commands'
 require 'account_component/account'
 require 'account_component/projection'
---
+++ lib/account_component/messages/commands/withdraw.rb
@@ -0,0 +1,9 @@
+module AccountComponent
+  module Messages
+    module Commands
+      class Withdraw
+        # Implement Withdraw command message
+      end
+    end
+  end
+end
