66aa4f480d0841cac1f2cd67233c392f1f540d6c
ntl <nathanladd+github@gmail.com>
2017-04-06 12:56:53 -0500

    Exercises use random account IDs

--- exercise/project_deposited.rb
+++ exercise/project_deposited.rb
@@ -1,17 +1,19 @@
 require_relative 'exercise_init'
 
+account_id = Identifier::UUID::Random.get
+
 deposited = Messages::Events::Deposited.new
-deposited.account_id = '123'
+deposited.account_id = account_id
 deposited.amount = 11
 deposited.time = '2000-01-01T11:11:11.00000Z'
 deposited.processed_time = Clock.iso8601
 
-stream_name = "account-123"
+stream_name = "account-#{account_id}"
 
 Messaging::Postgres::Write.(deposited, stream_name)
 
 account = Account.new
-account.id = '123'
+account.id = account_id
 
 puts "Account #{account.id} balance: $#{account.balance}"
 
