df2c68bbec40040ad0c9ba4052b30654a4cbdc20
ntl <nathanladd+github@gmail.com>
2017-04-07 15:48:23 -0500

    Starting point for store

--- exercise/exercise_init.rb
+++ exercise/exercise_init.rb
@@ -1,6 +1,6 @@
 ENV['CONSOLE_DEVICE'] ||= 'stdout'
 ENV['LOG_LEVEL'] ||= 'info'
-ENV['LOG_TAGS'] = '_untagged,-data,write,read,handle'
+ENV['LOG_TAGS'] ||= '_untagged,-data,write,read,handle,apply,entity_store'
 
 puts RUBY_DESCRIPTION
 
