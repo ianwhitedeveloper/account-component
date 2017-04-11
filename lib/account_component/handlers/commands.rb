38e51823be5df81635c4df47fd14654761daf81e
ntl <nathanladd+github@gmail.com>
2017-04-11 15:55:01 -0500

    Open command handler sets expected version

--- lib/account_component/handlers/commands.rb
+++ lib/account_component/handlers/commands.rb
@@ -21,7 +21,7 @@ module AccountComponent
       handle Open do |open|
         account_id = open.account_id
 
-        account = store.fetch(account_id)
+        account, version = store.fetch(account_id, include: :version)
 
         if account.open?
           logger.debug { "Command ignored (Command: #{open.message_type}, Account ID: #{account_id}, Customer ID: #{open.customer_id})" }
@@ -35,7 +35,7 @@ module AccountComponent
 
         stream_name = stream_name(account_id)
 
-        write.(opened, stream_name)
+        write.(opened, stream_name, expected_version: version)
       end
 
       handle Close do |close|
