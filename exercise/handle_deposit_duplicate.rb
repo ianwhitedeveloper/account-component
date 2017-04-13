e022cf78c299bb7552a1b450e23c3143515f1333
ntl <nathanladd+github@gmail.com>
2017-04-13 18:46:09 -0500

    Handle deposit duplicate exercise uses command and transaction handlers

--- exercise/handle_deposit_duplicate.rb
+++ exercise/handle_deposit_duplicate.rb
@@ -5,28 +5,36 @@ deposit_id = Identifier::UUID::Random.get
 
 deposit = Messages::Commands::Deposit.new
 deposit.account_id = account_id
-deposit.id = deposit_id
+deposit.deposit_id = deposit_id
 deposit.amount = 11
 deposit.time = '2000-01-01T11:11:11.00000Z'
 
 command_stream_name = "account:command-#{account_id}"
+transaction_stream_name = "accountTransaction-#{deposit_id}"
 
 
 store = Store.build
 
 
-Messaging::Postgres::Write.(deposit, command_stream_name)
-Messaging::Postgres::Write.(deposit, command_stream_name)
+2.times do
+  Messaging::Postgres::Write.(deposit, command_stream_name)
+end
 
 
 account = store.fetch(account_id)
-pp "Account #{account_id} balance: $#{account.balance}"
+puts "Account #{account_id} balance: $#{account.balance}"
 
 
 EventSource::Postgres::Read.(command_stream_name) do |event_data|
   Handlers::Commands.(event_data)
 end
 
+puts "Handling transaction commands"
+
+EventSource::Postgres::Read.(transaction_stream_name) do |event_data|
+  Handlers::Commands::Transactions.(event_data)
+end
+
 
 account = store.fetch(account_id)
-pp "Account #{account_id} balance: $#{account.balance}"
+puts "Account #{account_id} balance: $#{account.balance}"
